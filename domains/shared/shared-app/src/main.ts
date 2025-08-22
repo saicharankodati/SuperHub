89672beded9e7dd1775725640b32048b74f0a919
saicharankodati <31197027+saicharankodati@users.noreply.github.com>
2025-08-22 21:49:15 +0530

    Revert "updated shared remote"
    This reverts commit 7f0bbcff6518e8d72b8f7d8c4044ad139cb49dd4.

--- domains/shared/shared-app/src/main.ts
+++ domains/shared/shared-app/src/main.ts
@@ -1,6 +1,1 @@
-import { initFederation } from '@angular-architects/native-federation';
-
-initFederation()
-  .catch(err => console.error(err))
-  .then(_ => import('./bootstrap'))
-  .catch(err => console.error(err));
+export default void 0;
