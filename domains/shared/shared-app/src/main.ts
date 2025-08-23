6653625b478791ed8820348b5ed79c1828776d47
saicharankodati <31197027+saicharankodati@users.noreply.github.com>
2025-08-24 01:13:41 +0530

    initialized native federation in shared-app

--- domains/shared/shared-app/src/main.ts
+++ domains/shared/shared-app/src/main.ts
@@ -1,1 +1,6 @@
-export default void 0;
+import { initFederation } from '@angular-architects/native-federation';
+
+initFederation()
+  .catch(err => console.error(err))
+  .then(_ => import('./bootstrap'))
+  .catch(err => console.error(err));
