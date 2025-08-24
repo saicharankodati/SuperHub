19eaf5f36ac01281c79ae89cc2f4104cad755d94
saicharankodati <31197027+saicharankodati@users.noreply.github.com>
2025-08-24 23:03:35 +0530

    created website component in shell-app and changed route config

--- apps/shell-app/src/app/app.routes.ts
+++ apps/shell-app/src/app/app.routes.ts
@@ -1,22 +1,36 @@
 import { Routes } from '@angular/router';
 import { loadRemoteModule } from '@angular-architects/native-federation';
-import { App } from './app';
+import { Website } from './components/website/website';
 
 export const routes: Routes = [
     {
         path: '',
-        component: App
+        component: Website
     },
     {
         path: 'auth',
-        loadComponent: () => loadRemoteModule('auth-app', './Component').then((m) => m.App)
-    },
+        loadComponent: () => loadRemoteModule('auth-app', './Component').then((m) => m.App),
+        children: [
+            {
+                path: '',
+                loadComponent: () => loadRemoteModule('auth-app', './SignInComponent').then((m) => m.SignIn)
+            },
+            {
+                path: 'signin',
+                loadComponent: () => loadRemoteModule('auth-app', './SignInComponent').then((m) => m.SignIn)
+            },
+            {
+                path: 'signup',
+                loadComponent: () => loadRemoteModule('auth-app', './SignUpComponent').then((m) => m.SignUp)
+            }
+        ]
+    },    
     {
         path: 'dashboard',
         loadComponent: () => loadRemoteModule('dashboard-app', './Component').then((m) => m.App)
     },
     {
         path: '**',
-        component: App
+        component: Website
     }
 ];
