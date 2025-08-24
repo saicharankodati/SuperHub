3965ce12346b8dd36cdd9cc51e64bc1ae3a6a58b
saicharankodati <31197027+saicharankodati@users.noreply.github.com>
2025-08-24 23:11:44 +0530

    created sign-in, sign-up components in auth-app

---
+++ domains/auth/auth-app/src/app/components/sign-in/sign-in.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-in',
+  imports: [],
+  templateUrl: './sign-in.html',
+  styleUrl: './sign-in.scss'
+})
+export class SignIn {
+
+}
