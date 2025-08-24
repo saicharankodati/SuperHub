a93689d31eb43aa283365edb19321edd405a845c
saicharankodati <31197027+saicharankodati@users.noreply.github.com>
2025-08-24 22:52:48 +0530

    removed shared-app because we have some dependencies with vite causing remote loading issues for styles

--- apps/shell-app/src/main.ts
+++ apps/shell-app/src/main.ts
@@ -4,7 +4,3 @@ initFederation('federation.manifest.json')
   .catch(err => console.error(err))
   .then(_ => import('./bootstrap'))
   .catch(err => console.error(err));
-
-import('shared-app/Style').then(({ Style }) => {
-  Style();
-});
--- domains/shared/shared-app/src/styles/shared.style.ts
+++
@@ -1,9 +0,0 @@
-export function Style(): void {
-  const style = document.createElement('style');
-  style.innerHTML = `
-    h1 {
-        color: red;
-    }
-  `;
-  document.head.appendChild(style);
-}
