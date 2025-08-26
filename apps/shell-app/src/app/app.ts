6eca28056b47f86a5fdce60f2a148cadb36c4c5e
saicharankodati <saicharan.kodati@gmail.com>
2025-08-26 22:06:12 +0530

    applied shell-app code from old copy

--- apps/shell-app/src/app/app.ts
+++ apps/shell-app/src/app/app.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet, RouterLink, Router, Event, NavigationStart } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
-export class App {
-  protected title = 'shell-app';
+export class App implements OnInit {
+  appRootDiv: string = 'shell';
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationStart) {
+        switch(event.url) {
+          case '/signin':
+            this.router.navigate([`/auth${event.url}`]);
+            break;
+          case '/signup':
+            this.router.navigate([`/auth${event.url}`]);
+            break;
+          default:
+            break;
+        }
+      }
+    });
+    setTimeout(() => {
+      var appDiv = document.querySelector('div.app') as HTMLElement;
+      if(appDiv) {
+        appDiv.classList.add('active');
+      }
+      var logobgImg = document.querySelector('img.logo-bg') as HTMLElement;
+      if(logobgImg) {
+        logobgImg.classList.add('spin');
+      }
+      var shellNav = document.querySelector('nav.shell') as HTMLElement;
+      if(shellNav) {
+        shellNav.classList.add('active');
+      }
+    }, 100);
+  }
+
+  onSigninClick() {
+    var signinBtn = document.getElementById('nav-signin');
+    if(signinBtn) {
+      signinBtn.classList.remove('active');
+    }
+  }
 }
