94c1b3cae2579aa8ab3189a93e5e05d8dd652151
saicharankodati <31197027+saicharankodati@users.noreply.github.com>
2025-08-31 06:09:04 +0530

    added website component with routes to new shell-app

---
+++ apps/shell-app/src/app/components/website/website.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { Website } from './website';
+
+export const websiteRoutes: Routes = [
+    {
+        path: '',
+        component: Website
+    }
+];
---
+++ apps/shell-app/src/app/components/website/website.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-website',
+  imports: [],
+  templateUrl: './website.html',
+  styleUrls: ['./website.scss']
+})
+export class Website {
+
+}
