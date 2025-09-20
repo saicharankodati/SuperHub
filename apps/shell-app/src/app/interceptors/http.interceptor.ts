e58ac657cd93d52a099f06b19766491b8f554615
saicharankodati <saicharan.kodati@gmail.com>
2025-09-21 03:13:16 +0530

    altered UI/UX sizes based on small screen

--- apps/shell-app/src/app/interceptors/http.interceptor.ts
+++ apps/shell-app/src/app/interceptors/http.interceptor.ts
@@ -1,11 +1,24 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpRequest, HttpEvent, HttpHandlerFn } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
 import { signal_AccessToken } from '../signals/signals';
 
-export const HttpInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = signal_AccessToken();
-  const modifiedReq = token
-    ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } })
-    : req;
-
-  return next(modifiedReq);
+export const HttpInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> => {
+  const modifiedReq = handleRequest(req);
+  return next(modifiedReq).pipe(handleResponse());
 };
+
+function handleRequest(req: HttpRequest<any>): HttpRequest<any> {
+  const token = signal_AccessToken();
+  return token ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } }) : req;
+}
+
+function handleResponse<T extends HttpEvent<any>>() {
+  return tap<T>({
+    next: (event) => {
+      console.log('Response received:', event);
+    },
+    error: (err) => {
+      console.error('HTTP Error:', err);
+    }
+  });
+}
